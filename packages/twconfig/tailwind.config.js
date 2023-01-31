c00487b6c4f35358d34e018081a7b3af4263ad87
Akshay Jadhav <akshayjadhav461999@gmail.com>
2023-01-31 14:24:29 +0530

    tailwind css working

---
+++ packages/twconfig/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return <button className="px-4 py-2 bg-red-200 rounded-md">Boop</button>;
 };
