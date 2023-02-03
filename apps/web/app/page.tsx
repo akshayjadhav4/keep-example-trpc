e00d4d68b3620b7786d0a5827b7c65cb04919cf3
Akshay Jadhav <akshayjadhav461999@gmail.com>
2023-02-03 12:28:06 +0530

    app dir working

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1 className="text-center">Web</h1>
+      <h1 className="text-center text-red-700">Web</h1>
       <Button />
     </div>
   );
--- packages/twconfig/tailwind.config.js
+++ packages/twconfig/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "../../packages/ui/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
